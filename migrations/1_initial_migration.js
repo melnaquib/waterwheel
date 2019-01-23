c0bbb6b6d1ad4f9ce708006322faf45271458c15
Noah Zinsmeister <noah@hydrogenplatform.com>
2019-01-23 10:08:15 -0500

    fix path

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -5,8 +5,8 @@ const StringUtils = artifacts.require('./_testing/ClientRaindrop/StringUtils.sol
 const HydroToken = artifacts.require('./_testing/HydroToken.sol')
 const IdentityRegistry = artifacts.require('./_testing/IdentityRegistry.sol')
 const Snowflake = artifacts.require('./_testing/Snowflake.sol')
-const OldClientRaindrop = artifacts.require('./_testing/OldClientRaindrop.sol')
-const ClientRaindrop = artifacts.require('./resolvers/ClientRaindrop/ClientRaindrop.sol')
+const OldClientRaindrop = artifacts.require('./_testing/ClientRaindrop/OldClientRaindrop.sol')
+const ClientRaindrop = artifacts.require('./_testing/ClientRaindrop/ClientRaindrop.sol')
 
 // link libraries
 module.exports = async function(deployer) {
